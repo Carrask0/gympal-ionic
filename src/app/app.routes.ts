a0983f9c076d5120b9e97baaae1a44ee261fd63c
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2024-03-04 17:32:47 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'principal',
+    loadComponent: () => import('./pages/principal/principal.page').then( m => m.PrincipalPage)
+  },
 ];
