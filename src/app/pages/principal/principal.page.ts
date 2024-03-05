e5d74183cc2a780487a9c8c083c636a070e4473d
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2024-03-05 12:48:45 +0100

    done main page layout

--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
@@ -12,9 +13,39 @@ import { IonicModule } from '@ionic/angular';
 })
 export class PrincipalPage implements OnInit {
 
+  user: any //TODO: create user model
+
   constructor() { }
 
   ngOnInit() {
+
+    // Probably receives user info from a login process
+    // Or we assume the user is already logged in and we have only one user for this demo
+
+    //mock user
+    this.user = {
+      id: 1,
+      name: "John Doe",
+      profile_pic: "assets/imgs/profile_pic.png",
+      goalPoints: 210,
+      todayPoints: 30,
+      todaySessions: [
+        {
+          id: 1,
+          name: "Sesión 1"
+        },
+        {
+          id: 2,
+          name: "Sesión 2"
+        },
+        {
+          id: 3,
+          name: "Sesión 3"
+        }
+      ]
+
+
+    }
   }
 
 }
--- src/app/pages/sesion/sesion.page.ts
+++ src/app/pages/sesion/sesion.page.ts
@@ -12,9 +12,52 @@ import { IonicModule } from '@ionic/angular';
 })
 export class SesionPage implements OnInit {
 
+  session: any; //TODO: create session model
+  editMode: boolean = false;
+  date: string = "01/01/2021";
+
   constructor() { }
 
   ngOnInit() {
+
+    this.editMode = false;
+
+    this.date = new Date().toISOString();
+    //format date to dd/mm/yyyy
+    let date = new Date(this.date);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    this.date = day + "/" + month + "/" + year;
+
+
+    this.session = {
+      sessionName: "Session 1",
+      sessionDate: "1/1/2021",
+      puntos: 0,
+      kgsMovidos: 0,
+      kcalsQuemadas: 0,
+      exercises: [
+        {
+          exerciseName: "Press de banca",
+          kgs: 80,
+          reps: 10,
+          sets: 3,
+        },
+        {
+          exerciseName: "Sentadillas",
+          kgs: 100,
+          reps: 8,
+          sets: 5,
+        },
+        {
+          exerciseName: "Dominadas",
+          kgs: 0,
+          reps: 10,
+          sets: 3,
+        },
+      ],
+    };
   }
 
 }
